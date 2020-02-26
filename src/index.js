b4d0b244795a3bd14c00c38bad25a5d30af850c0
Student <student@PSXGA125s-MacBook-Air.local>
2020-02-26 08:53:06 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import '@appbaseio/reactivesearch/dist/css/style.min.css';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Routes from './Routes';
+//import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Routes />, document.getElementById('root'));
+//registerServiceWorker();
